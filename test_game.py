b0fcb9fe2213cf077e5ceeba72ad3e6fda91039f
si <psiluv@gmail.com>
2025-07-31 14:46:49 +0900

    [refactoring]59

--- test_game.py
+++ test_game.py
@@ -12,6 +12,13 @@ def assert_illegal_input(game, inputs):
     with pytest.raises(TypeError):
         game.guess(inputs)
 
+
+def assert_matched_number(result, solved, strikes, balls):
+    assert result is not None
+    assert result.solved == solved
+    assert result.strikes == strikes
+    assert result.balls == balls
+
 @pytest.mark.parametrize("invalid_input", [None, "12", "1245", "12d", "121"])
 def test_exception_when_invalid_inputs(game, invalid_input):
     assert_illegal_input(game, invalid_input)
@@ -19,17 +26,9 @@ def test_exception_when_invalid_inputs(game, invalid_input):
 
 def test_return_solved_result_if_matched_number(game):
     game.question = '123'
-    result:GameResult = game.guess("123")
-    assert result is not None
-    assert result.solved == True
-    assert result.strikes == 3
-    assert result.balls == 0
+    assert_matched_number(game.guess("123"), True, 3, 0)
 
 
 def test_return_solved_result_if_unmatched_number(game):
     game.question = '123'
-    result:GameResult = game.guess("456")
-    assert result is not None
-    assert result.solved == False
-    assert result.strikes == 0
-    assert result.balls == 0
+    assert_matched_number(game.guess("456"), False, 0, 0)
